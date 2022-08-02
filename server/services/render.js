ef1913592d7b011f0c1afa88f19e078ed4c5820e
Andrea Watson <awatson.e@gmail.com>
2022-08-02 15:41:18 -0400

    pushing to heroku

--- server/services/render.js
+++ server/services/render.js
@@ -10,7 +10,7 @@ exports.intro = (req, res) => {
 
 //renders the interactive map page
 exports.loadAllSpecies = (req, res) => {
-    axios.get(`http://localhost:4747/api/v1/species`)
+    axios.get(`${process.env.API_URL}/api/v1/species`)
         .then((response) => {
             //console.log(response.data)
             res.render('database_map', { species: JSON.stringify(response.data).replace(/'/g, '\\u0027') })
@@ -22,7 +22,7 @@ exports.loadAllSpecies = (req, res) => {
 
 //renders the species list page
 exports.loadSpeciesList = (req, res) => {
-    axios.get(`http://localhost:4747/api/v1/species`)
+    axios.get(`${process.env.API_URL}/api/v1/species`)
         .then((response) => {
             res.render('species_list', { species: response.data })
         })
@@ -33,8 +33,8 @@ exports.loadSpeciesList = (req, res) => {
 
 //renders the species specific page
 exports.loadSpecies = (req, res) => {
-    const getSpecies = axios.get(`http://localhost:4747/api/v1/species/${req.params.speciesName}`)
-    const getEpisodes = axios.get(`http://localhost:4747/api/v1/episodes/${req.params.speciesName}`)
+    const getSpecies = axios.get(`${process.env.API_URL}/api/v1/species/${req.params.speciesName}`)
+    const getEpisodes = axios.get(`${process.env.API_URL}/api/v1/episodes/${req.params.speciesName}`)
     Promise.all([getSpecies, getEpisodes])
         .then((response) => {
             res.render('species_info', { episodes: response[1].data, species: response[0].data, speciesName: req.params.speciesName})
@@ -46,7 +46,7 @@ exports.loadSpecies = (req, res) => {
 
 //renders the add an episode form page
 exports.addEpisode = (req, res) => {
-    axios.get(`http://localhost:4747/api/v1/species`)
+    axios.get(`${process.env.API_URL}/api/v1/species`)
     .then((response) => {
         let names = response.data.map((alien) => {
             return alien.name
